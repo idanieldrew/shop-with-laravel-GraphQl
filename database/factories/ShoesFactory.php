738c019ab184b7806c3e463dd6c0ca2d95663137
idanieldrew <danielgia35@gmail.com>
2021-06-10 16:12:21 +0430

    start project

---
+++ database/factories/ShoesFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Shoes;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ShoesFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Shoes::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_06_10_112713_create_shoes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShoesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shoes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('color');
+            $table->string('size');
+            $table->string('image')->nullable();
+            $table->string('details');
+            $table->text('description');
+            $table->boolean('featured')->default(false);
+            $table->string('link')->nullable();
+
+            $table->foreignId('category_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shoes');
+    }
+}
